bda4e93c2bec873eca4618232e4f119052cb85a7
Omprakash <onarpaladevops@gmail.com>
2025-05-21 13:33:02 +0530

    variable arguments

--- 004_variables_arguments.sh
+++ 004_variables_arguments.sh
@@ -8,3 +8,6 @@ echo "$PERSON2:: Hello $PERSON1, I am fine. How are you doing"
 echo "$PERSON1: I am fine too. What's up?"
 echo "$PERSON2:: Nohting, just going to Mars now, will you come?"
 echo "$PERSON1:: Sorry, you carry on! I will come once you come back"
+
+
+echo "Script Name: $0"
