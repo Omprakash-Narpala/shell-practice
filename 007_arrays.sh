cde57439a6082dd594cfde4392e4835ea94882ef
Omprakash <onarpaladevops@gmail.com>
2025-05-21 14:05:27 +0530

    arrays

--- 007_arrays.sh
+++ 007_arrays.sh
@@ -3,3 +3,6 @@
 SUBJECTS=("Maths" "Hindi" "Telugu" "English")
 
 echo "Favorite subject: ${SUBJECTS[0]}"
+echo "First language: ${SUBJECTS[2]}"
+echo "Second language: ${SUBJECTS[1]}"
+echo "Teaching language: ${SUBJECTS[3]}"
