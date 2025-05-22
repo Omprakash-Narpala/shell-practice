d8ef98c5baae9c3c835f14b6e757b1729fabc9f0
Omprakash <onarpaladevops@gmail.com>
2025-05-22 21:39:37 +0530

    Colors

---
+++ 013_colors.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+USERID=$(id -u)
+if [ $USERID -ne 0 ]
+    then
+        echo -e "\e[31mError: Please run the script with root access\e[0m"
+        exit 1
+    else
+        echo -e "\e[34You are running with root access\e[0m"
+fi
+
+VALIDATE_PACKAGE ()         #Defined function
+{
+    if [ $1 -eq 0 ]
+        then
+            echo -e "\e[32m$2 Installation is ... SUCCESS\e[0m"
+        else
+            echo -e "\e[31m$2 Installation is ... FAILURE\e[0m"
+            exit 1
+    fi
+}
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+    then
+        echo -e "\e[36mMySQL is not installed.. going to install it\e[0m"
+        dnf install mysql -y
+        VALIDATE_PACKAGE $? "MySQL"
+    else
+        echo -e "\e[34mMySQL is already installed. Nothing to do.\e[0m"
+fi
+
+dnf list installed python
+
+if [ $? -ne 0 ]
+    then
+        echo -e "\e[36mPython is not installed.. going to install it\e[0m"
+        dnf install python -y
+        VALIDATE_PACKAGE $? "Python"
+    else
+        echo -e "\e[34mPython is already installed. Nothing to do.\e[0m"
+fi
+
+dnf list installed nginx
+
+if [ $? -ne 0 ]
+    then
+        echo -e "\e[36mNginx is not installed.. going to install it\e[0m"
+        dnf install nginx -y
+        VALIDATE_PACKAGE $? "Nginix"
+    else
+        echo -e "\e[34mNginx is already installed. Nothing to do.\e[0m"
+fi
