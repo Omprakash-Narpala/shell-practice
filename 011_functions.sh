54e34cec44ca1ac6de1a62deea002533457e3480
Omprakash <onarpaladevops@gmail.com>
2025-05-22 16:54:40 +0530

    Functions

---
+++ 011_functions.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+USERID=$(id -u)
+if [ $USERID -ne 0 ]
+    then
+        echo "Please run the script with root access"
+    else
+        echo "You are running with root access"
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+    then
+        echo "MySQL is not installed.. going to install it"
+        dnf install mysql -y
+        if [ $? -eq 0 ]
+        then
+            echo "MySQL Installation is ... SUCCESS"
+        else
+            echo "MySQL Installation is ... FAILURE"
+            exit 1
+        fi
+    else
+        echo "MySQL is already installed. Nothing to do."
+fi
