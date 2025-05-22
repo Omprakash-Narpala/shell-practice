319a7c63a4d0e5978c4be656615ef74856bb0232
Omprakash <onarpaladevops@gmail.com>
2025-05-22 16:37:06 +0530

    Install MySQL practice

---
+++ 010_install_practice.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+USERID=$(id -u)
+if [ $USERID -ne 0 ]
+    then
+        echo "Please run the script with root access"
+        exit 1
+    else
+        echo "You are running with root access"
+fi
+
+dnf list installed mysql
+
+if [ $? -eq 0 ]
+    then
+        echo "MySQL is already installed. Nothing to do."
+    else
+        dnf install mysql -y
+        if [ $? -eq 0 ]
+            then
+                echo "Installing MySQL is... SUCCESS"
+            else
+                echo "Installing MYSQL is... FAILURE"
+                exit 1
+        fi 
+fi
