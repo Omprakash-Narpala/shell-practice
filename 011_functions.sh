7da73b9208cf19bfd209b109f2dbcecd920a6f98
Omprakash <onarpaladevops@gmail.com>
2025-05-22 17:30:47 +0530

    Functions

--- 011_functions.sh
+++ 011_functions.sh
@@ -24,3 +24,37 @@ if [ $? -ne 0 ]
     else
         echo "MySQL is already installed. Nothing to do."
 fi
+
+dnf list installed python
+
+if [ $? -ne 0 ]
+    then
+        echo "Python is not installed.. going to install it"
+        dnf install python -y
+        if [ $? -eq 0 ]
+        then
+            echo "Python Installation is ... SUCCESS"
+        else
+            echo "Python Installation is ... FAILURE"
+            exit 1
+        fi
+    else
+        echo "Python is already installed. Nothing to do."
+fi
+
+dnf list installed nginx
+
+if [ $? -ne 0 ]
+    then
+        echo "Nginx is not installed.. going to install it"
+        dnf install nginx -y
+        if [ $? -eq 0 ]
+        then
+            echo "Nginx Installation is ... SUCCESS"
+        else
+            echo "Nginx Installation is ... FAILURE"
+            exit 1
+        fi
+    else
+        echo "Nginx is already installed. Nothing to do."
+fi
