faf4fe1f15e9ead562aa33d0e89ce8226527c950
Omprakash <onarpaladevops@gmail.com>
2025-05-22 17:17:00 +0530

    Functions

--- 011_functions.sh
+++ 011_functions.sh
@@ -2,7 +2,7 @@
 USERID=$(id -u)
 if [ $USERID -ne 0 ]
     then
-        echo "Please run the script with root access"
+        echo "Error: Please run the script with root access"
         exit 1
     else
         echo "You are running with root access"
